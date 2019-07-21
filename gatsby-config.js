66dedc680138b8662da2223e44207c9fc974bf62
小林杏理 <aanrii@aanrii-mbp-2094.local>
2019-07-22 00:21:54 +0900

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
@@ -23,5 +17,10 @@ module.exports = {
       },
     },
     `gatsby-remark-grid-tables`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-144247191-1",
+      },
   ],
 }
